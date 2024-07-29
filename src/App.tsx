24fdccc03b4cb6f05df24a7a20455d023f9f33ac
CleidsonGoes <jorgegoes4@gmail.com>
2024-07-29 18:39:27 -0300

    alteração de componentes e criação do comp. input

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Table from './components/Table'
+import Input from './components/Input';
+
+const App = () => {
+    return (
+        <div>
+            <Input />
+            <Table />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Input.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+  
+const Input: React.FC = () => {
+ 
+    return (
+      <input>Entrada</input>
+    );
+  };
+  
+  export default Input;
